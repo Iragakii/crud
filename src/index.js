10e7e0ce73ad7911a07a2756b3f28d3a68091a99
unknown <jcgroup.vni@gmail.com>
2024-07-25 13:10:13 +0700

    crud

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Footer, Navbar } from './pages/Layout';
+import { Home } from './pages/Home';
+import { Products } from './pages/Product';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+function App(){
+  return(
+    <>
+    <BrowserRouter>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}/>
+        <Route path='/products' element={<Products></Products>}/>
+      </Routes>
+     
+      <Footer></Footer>
+      </BrowserRouter>  
+    </>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +28,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
